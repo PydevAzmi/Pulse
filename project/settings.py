b05e736ed69ad2293222e546e169aff05a3592cc
pydevazmi <pydevazmi@gmail.com>
2023-03-23 02:02:51 +0200

    changes added to project settings

--- project/settings.py
+++ project/settings.py
@@ -39,9 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # My Apps
     "settings",
+    "consultation",
 
 ]
 
