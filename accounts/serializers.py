44d13381f5f9a063a146a32d8004743e7fb64c8a
pydevazmi <pydevazmi@gmail.com>
2023-04-14 18:00:03 +0200

    Editing Serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -23,7 +23,6 @@ def handle_uploaded_file(file):
             destination.write(chunk)
     return file.name
 
-
 class PatientSignUpSerializer(RegisterSerializer):
     first_name = serializers.CharField( write_only=True, required=True,max_length = 50)
     last_name = serializers.CharField(max_length = 50)
@@ -113,48 +112,6 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ["id", "first_name", "last_name", "username"]
-
-class LoginSerializer(serializers.Serializer):
-
-    username = serializers.CharField(
-        label="Username",
-        write_only=True
-    )
-    password = serializers.CharField(
-        label="Password",
-        # This will be used when the DRF browsable API is enabled
-        style={'input_type': 'password'},
-        trim_whitespace=False,
-        write_only=True
-    )
-
-    def validate(self, attrs):
-        # Take username and password from request
-        username = attrs.get('username')
-        password = attrs.get('password')
-
-        if username and password:
-           
-            user = authenticate(request=self.context.get('request'),
-                                username=username, password=password)
-            if not user:
-                # If we don't have a regular user, raise a ValidationError
-                msg = 'Access denied: wrong username or password.'
-                raise serializers.ValidationError(msg, code='authorization')
-        else:
-            msg = 'Both "username" and "password" are required.'
-            raise serializers.ValidationError(msg, code='authorization')
-        # We have a valid user, put it in the serializer's validated_data.
-        # It will be used in the view.
-        attrs['user'] = user
-        return attrs
-
-
-
-
-
-
-
 
 
 
