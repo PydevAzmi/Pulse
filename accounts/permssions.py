fb5c0eb02b94f5ff1bacc82180bdac7459f132f7
pydevazmi <pydevazmi@gmail.com>
2023-04-25 17:37:12 +0200

    Editing Accounts Permissions

--- accounts/permssions.py
+++ accounts/permssions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from .models import Patient, Doctor
 class IsDoctorOrReadOnly(permissions.BasePermission):
     """
     Check if Doctor is Owner of the Profile.
@@ -10,7 +10,8 @@ class IsDoctorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user 
+        patient = Patient.objects.get(user = request.user  )
+        return obj.patient == patient
 
 class IsPatientOrReadOnly(permissions.BasePermission):
     """
@@ -22,4 +23,5 @@ class IsPatientOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.user == request.user 
+        doctor = Doctor.objects.get(user = request.user)
+        return obj.doctor == doctor
