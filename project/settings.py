0837009baa1702b68540b2a017864f808226e1ed
pydevazmi <pydevazmi@gmail.com>
2023-04-26 11:42:25 +0200

    Adding and modifing files for deployment

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-85rx&vrcl9ikf=8mmza@(dkyqv%beqs*8h56eg#c&(r8pufkl4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
