084d4dde4cf157c501d0016517b27e946eaf75ef
pydevazmi <pydevazmi@gmail.com>
2023-04-27 05:33:01 +0200

    procfile and collectstatic for deployment

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
 
     # My Apps
@@ -145,14 +146,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR /"static/"
-]
+STATICFILES_DIRS = [BASE_DIR /"static/"]
 
-'''
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-'''
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL  = 'media/'
 MEDIA_ROOT  = BASE_DIR / 'media'
