6ef904c08fc829ee7e87d412bfc10a63cf97256a
pydevazmi <pydevazmi@gmail.com>
2023-05-31 19:27:18 +0300

    Test New ECG Model With High Accuracy

--- testmodel.py
+++ testmodel.py
@@ -1,30 +1,33 @@
+import cv2 
+import numpy as np
 import tensorflow as tf
 from keras.models import load_model
 
-mri_labels = {1: "normal", 0: "abnormal"}
-
 # Load the saved model
-model = load_model('MRI_MODEL.h5')
+model = load_model('F:\Githup Repos\Pulse\model.h5')
 
 # Load the image you want to classify
-img = tf.keras.preprocessing.image.load_img('media\IM00002.jpg',
-                                             target_size=(224, 224))
+image_path = "media\patients\john/ahmed\Scans/abnormal100.jpg"
 
 # Preprocess the image
-x = tf.keras.preprocessing.image.img_to_array(img)
-x = x / 255.0
-x = tf.expand_dims(x, axis=0)
+def preprocess_data(image_path):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (240, 200))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=-1)
+    return img
 
 # Make a prediction
-predictions = model.predict(x)
-mri_prediction = model.predict(x)
+def predict_image(model, image_path):
+    preprocessed_image = preprocess_data(image_path)
+    predictions = model.predict(np.array([preprocessed_image]))
+    class_labels = ['Normal', 'Myocardial Infarction', 'History of MI', 'Abnormal Heartbeat']
+    predicted_label = class_labels[np.argmax(predictions)]
+    return predicted_label
 
+predicted_image = predict_image(model,image_path)
 # Print the predicted class
-predicted_class = tf.argmax(predictions, axis=1)
-mri_prediction = mri_prediction.argmax(axis = 1)
-mri_prediction = mri_labels[mri_prediction[0]]
-
-print(predicted_class)
-print(mri_prediction)
+print(predicted_image)
 
 
